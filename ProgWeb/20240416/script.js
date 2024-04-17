ba556ed0ab4d4683bca0ef81e2853c35e5652d57
eduardolcsg <eduardolcsg@gmail.com>
2024-04-16 21:25:11 -0400

    up

---
+++ ProgWeb/20240416/script.js
@@ -0,0 +1,6 @@
+function mouseover(){
+    document.getElementById("teste").style.backgroundColor="red";
+}
+function mouseout(){
+    document.getElementById("teste").style.backgroundColor="green"
+}
